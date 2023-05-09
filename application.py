ecc0b0cf875a7616fbab17a84a5710e551550abe
Colin FitzGerald <colinfitzgerald328@gmail.com>
2023-05-08 20:55:31 -0700

    add latest:

--- application.py
+++ application.py
@@ -1,9 +1,9 @@
 from flask import Flask
 
 # EB looks for an 'application' callable by default.
-blog_server = Flask(__name__)
+application = Flask(__name__)
 
-@blog_server.route('/', methods=['GET'])
+@application.route('/', methods=['GET'])
 def get_athlete_history():
     return {"operation": "success",
             "server_status": "operational"
@@ -11,7 +11,4 @@ def get_athlete_history():
 
 # run the app.
 if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
-    blog_server.debug = True
-    blog_server.run()
+    application.run()
